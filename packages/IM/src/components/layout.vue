97aece8a6341d78fe07139dee6bcfc2cd26aac3b
吴东 <511970747@qq.com>
2022-11-11 16:38:27 +0800

    feat: 静态UI

--- packages/IM/src/components/layout.vue
+++ packages/IM/src/components/layout.vue
@@ -55,10 +55,10 @@ export default {
     //
     // })
     //
-    // IMSDK.registerEvent(IMSDK.EVENTS.ON_RECEIVED_MESSAGE, (e) => {
-    //   //事件处理
-    //   console.log("收到消息：", e.data)
-    // })
+    IMSDK.registerEvent(IMSDK.EVENTS.ON_RECEIVED_MESSAGE, (e) => {
+      //事件处理
+      console.log("收到消息：", e.data)
+    })
     //
     // IMSDK.registerEvent(IMSDK.EVENTS.ON_EVENT_SYNC_START, () => {
     //   console.log("收取中... ")
