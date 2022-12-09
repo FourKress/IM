c08f6e35ed2dfd33e319d348b13e6752f3757c9c
吴东 <511970747@qq.com>
2022-12-09 11:47:31 +0800

    feat: init

--- packages/utils/src/main.js
+++ packages/utils/src/main.js
@@ -1,6 +1,20 @@
 import msgTypeMap from './msgTypeMap';
 import timesTransform from './times-transform';
 import emojiList from './emoji';
-import { _throttle } from './lodash';
+import lodash, { _throttle } from './lodash';
+import { _clearSessionUnreadCount } from './im-utils';
+import generateRoute from './router';
+import generateStore from './store';
+import * as token from './token';
 
-export { msgTypeMap, timesTransform, emojiList, _throttle };
+export {
+  msgTypeMap,
+  timesTransform,
+  emojiList,
+  lodash,
+  _throttle,
+  _clearSessionUnreadCount,
+  generateRoute,
+  generateStore,
+  token,
+};
