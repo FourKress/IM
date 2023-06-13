3f5973708fb6c1cc69b580ad2335c2a5ee5b233e
吴东 <511970747@qq.com>
2023-06-13 19:02:57 +0800

    fix: bug

--- play/generate-install.js
+++ play/generate-install.js
@@ -2,21 +2,24 @@ const child_process = require('child_process');
 const path = require('path');
 const fse = require('fs-extra');
 
+const cwdPath = path.join(process.cwd(), '/custom-install');
 const installOriginPath = path.join(process.cwd(), '/build/win-unpacked');
-const installTargetPath = path.join(
-  process.cwd(),
-  '/custom-install/FilesToInstall',
-);
+const installTargetPath = path.join(cwdPath, '/FilesToInstall');
+
 fse.copySync(installOriginPath, installTargetPath);
 
-const batPath = path.join(process.cwd(), '/custom-install/build-nim.bat');
-const bat = child_process.spawn(batPath);
+const batPath = path.join(cwdPath, '/build-nim.bat');
+const bat = child_process.spawn(batPath, {
+  cwd: cwdPath,
+});
+
+console.log('自定义安装包 生成中 请等待...');
 
 bat.on('exit', (code) => {
   console.log(`Child exited with code ${code}`);
   if (code === 0) {
     console.log('自定义安装包 生成完毕。');
-    console.log('路径:', path.join(batPath, '../Output'));
+    console.log('路径:', path.join(cwdPath, '/Output'));
   } else {
     console.log('自定义安装包 生成失败。');
   }
