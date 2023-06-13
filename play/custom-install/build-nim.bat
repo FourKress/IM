49f083d689c982a054bde7b7acb31d3992fd6e61
吴东 <511970747@qq.com>
2023-06-13 17:46:07 +0800

    fix: bug

--- play/custom-install/build-nim.bat
+++ play/custom-install/build-nim.bat
@@ -1,8 +1,40 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @call makeapp.bat
 
 @call makeskinzip.bat nim
 
-echo %cd%
-echo %0
+".\NSIS\makensis.exe" ".\SetupScripts\nim\nim_setup.nsi"
 
-"%cd%\custom-install\NSIS\makensis.exe" "%cd%\custom-install\SetupScripts\nim\nim_setup.nsi"
+@rem 如果要调试错误，请使用下面的脚本，这样会打开编译界面（命令行界面中文会显示成?号）
+@rem ".\NSIS\makensisw.exe" ".\SetupScripts\nim\nim_setup.nsi"
