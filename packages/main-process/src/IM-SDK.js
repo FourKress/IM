cf4e896a37d46403f047da7ef09ab347df9632bd
吴东 <511970747@qq.com>
2023-08-11 14:59:00 +0800

    fix: 修改sdk日志等级

--- packages/main-process/src/IM-SDK.js
+++ packages/main-process/src/IM-SDK.js
@@ -21,7 +21,7 @@ export const IMSDKInit = (appId) => {
   global.IMSDK = IMSDK;
 
   IMSDK.getMainProvider().setLogLevel(
-    process.env.WEBPACK_DEV_SERVER_URL ? LogLevel.INFO : LogLevel.INFO,
+    process.env.WEBPACK_DEV_SERVER_URL ? LogLevel.ERROR : LogLevel.INFO,
   );
 
   IMSDK.getMainProvider().setNetworkChangeCallBack((state) => {
