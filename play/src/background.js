24e6ef78996f2cfe434c7fa0d2fb938294f0d58f
吴东 <511970747@qq.com>
2023-08-25 15:43:26 +0800

    fix: 关闭调试模式

--- play/src/background.js
+++ play/src/background.js
@@ -3,7 +3,7 @@ import { initElectron, initIpcMain } from '@lanshu/main-process';
 initElectron({
   terminal: 'government',
   isDevtools: false,
-  version: '0.4.10',
+  version: '0.0.1',
 }).then(() => {
   initIpcMain();
 });
